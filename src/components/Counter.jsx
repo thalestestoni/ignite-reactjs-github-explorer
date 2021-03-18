ab588130cea9f3dedc7e0d0ba424ca8c5179efd8
Thales Testoni <thalestestoni@gmail.com>
2021-03-18 20:19:12 -0300

    Estado do componente

---
+++ src/components/Counter.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+export function Counter() {
+    const [counter, setCounter] = useState(0);
+
+    function increment() {
+        setCounter(counter + 1);
+    }
+
+    return (
+        <div>
+            <h1>{counter}</h1>
+            <button type="button" onClick={increment}>
+                Increment
+            </button>
+        </div>
+    );
+}
